f850aa8f2bbed2c734cb79d64a625d4811f07528
riteshhjp <ritesh.hjp129@gmail.com>
2025-06-25 11:18:45 +0530

    FullStack

---
+++ java script/Module 1/console/log.js
@@ -0,0 +1,4 @@
+console.log(1);
+console.log("Hello World");
+
+console.log(1, "Hello World");
---
+++ java script/Module 2/Sumbols.js
@@ -0,0 +1,7 @@
+let sym1 = Symbol();
+
+let sym2 = Symbol('test');
+
+let sym3 = Symbol('test');
+
+console.log(sym2 === sym3);
